41fb8dc67cb945d4490b74cf5a0e0c7f1b72b64d
Zhaoyang Chen <31273076+CZhaoYoung@users.noreply.github.com>
2021-12-14 22:44:50 -0800

    每日学习12-14-2021

--- learn-psycopg2.py
+++ learn-psycopg2.py
@@ -1,16 +1,35 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname = testdb")
+conn = psycopg2.connect("dbname = postgres user = postgres password = 9817")
 
 cursor = conn.cursor()
 
-cursor.excute('''
+cursor.execute('''
+    DROP TABLE IF EXISTS test;
+''')
+
+cursor.execute('''
     CREATE TABLE test (
-    id serial PRIMARY KEY,
-    description VARCHAR NOT NULL
+    id INTEGER PRIMARY KEY,
+    completed BOOLEAN NOT NULL DEFAULT False
     );
 ''')
 
+cursor.execute('INSERT INTO test (id, completed) VALUES (%s, %s);', (1, True))
+
+SQL = 'INSERT INTO test (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+    'id': 2,
+    'completed': False
+}
+
+cursor.execute('SELECT * from test')
+result = cursor.fetchall()
+print(result)
+
+
+cursor.execute(SQL, data)
 conn.commit()
 
 cursor.close()
